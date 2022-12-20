0e83e18548edc8c1611460a4d5c5810aa1e71bab
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-12-19 16:57:54 -0800

    I added a small little form and learned about v-model

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    <input type="text" placeholder="name"/> 
+    <input type="text" placeholder="name" v-model="name"/> 
+
+  </div>
+  <div>
+    <input type="text" placeholder="email"/>
   </div>
   <div>
     <button type="submit">Submit</button>
   </div>
+  <router-view>
+
+  </router-view>
+    
 </template>
 
-<script></script>
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'MyForm',
+  setup() {
+    const name = ref('')
+    const email = ref('')
+    const submit = () => {
+      console.log(name.value, email.value)
+    }
+    return {
+      name,
+      email,
+      submit
+    }
+  }
+}
+</script>
 
-<style></style>
+<style>
+input[ type="text"]{
+  padding: 20px;
+  margin-bottom: 0.4ch;
+}
+
+button[type="submit"]{
+  padding: 20px;
+  margin-top: 0.4ch;
+  background-color: #000;
+  color: #fff;
+}
+</style>
