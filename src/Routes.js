76145c1b70edbafa1fa8a7440a761ff87aa49b69
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-12-25 18:00:58 -0800

    starting routing i'm very sick.. hard to think

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/components/Home.vue')
+  },
+  {
+    path: '/about',
+    component: () => import('@/components/About.vue')
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
